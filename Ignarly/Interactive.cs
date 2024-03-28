b7bf390ab4e2f67ac6dfa591a2a2f3c49aec4cc1
Jon Naske <jon@shorecel.com>
2024-03-28 09:38:36 -0700

    Add interactive template selection

--- Ignarly/Interactive.cs
+++ Ignarly/Interactive.cs
@@ -1,6 +1,18 @@
-﻿namespace Ignarly;
+﻿using Spectre.Console;
 
-public class Interactive
+namespace Ignarly;
+
+public static class Interactive
 {
-
+    static public string SelectTemplate(IEnumerable<string> templateList)
+    {
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .AddChoices(templateList)
+                .Title("Select a .gitignore template")
+                .EnableSearch()
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more choices)[/]")
+        );
+    }
 }
--- Ignarly/Program.cs
+++ Ignarly/Program.cs
@@ -1,27 +1,13 @@
-﻿using Octokit;
-using Spectre.Console;
+﻿using Ignarly;
+using Octokit;
 
+var client = new GitHubClient(new ProductHeaderValue("Ignarly"));
+var templateList = await client.GitIgnore.GetAllGitIgnoreTemplates();
 
-var github = new GitHubClient(new ProductHeaderValue("Ignarly"));
-var contents = await github.Repository.Content.GetAllContents("github", "gitignore", "/");
+var selection = Interactive.SelectTemplate(templateList);
+Console.WriteLine($"You have selected: {selection}");
 
-var gitignoreFiles = contents.Where(c => c.Name.EndsWith(".gitignore")).ToList();
-var fileListNoExtension = gitignoreFiles.Select(f => f.Name.Replace(".gitignore", "")).ToList();
+var gitignoreContent = await client.GitIgnore.GetGitIgnoreTemplate(selection);
 
-var selection = AnsiConsole.Prompt(
-    new SelectionPrompt<string>()
-        .Title("Select a .gitignore template")
-        .EnableSearch()
-        .PageSize(10)
-        .MoreChoicesText("[grey](Move up and down to reveal more choices)[/]")
-        .AddChoices(fileListNoExtension)
-);
-Console.WriteLine($"You selected: {selection}");
-
-var gitignoreFile = contents.First(c => c.Name == $"{selection}.gitignore");
-var httpClient = new HttpClient();
-var gitignoreContent = await httpClient.GetStringAsync(gitignoreFile.DownloadUrl);
-Console.WriteLine(gitignoreContent);
 var fileWriter = new StreamWriter("test.gitignore");
-fileWriter.Write(gitignoreContent);
-Console.ReadKey();
+fileWriter.Write(gitignoreContent.Source);
